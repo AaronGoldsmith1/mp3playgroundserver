0895129ead7a348ed362149a7ea7972912389403
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-12-10 20:58:10 -0800

    update playlist controller

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -1,4 +1,5 @@
 var User = require('../models/User');
+var _ = require('lodash');
 
 module.exports = {
   index: index,
@@ -13,17 +14,47 @@ function index (req, res, next) {
 }
 
 function create(req, res, next) {
+  var playlist = {title: req.body.title, description: req.body.description, songs: []}
 
+  User.findByIdAndUpdate(
+    req.authenticatedUser._id,
+    { $push : {'playlists': playlist}},
+    { safe: true, new: true },
+    function(err, user){
+      if (err) return console.log(err)
+
+      var newPlaylist = _.find(user.playlists, playlist);
+
+      res.json(newPlaylist);
+    });
 }
 
-function show(req, res, next) {
+function show(req, res, next){
+  console.log("Playlists::show", req.params.id)
+  var playlist = _.find(req.authenticatedUser.playlists,
 
-}
+  function(o){
+    return o._id == req.params.id
+  });
+
+  res.json(playlist);
+};
 
 function update(req, res, next) {
 
+      //$push, $pull songs 
+
+
 }
 
-function destroy(req, res, next) {
-  
+function destroy(req, res, next){
+    var playlist = {title: req.params.title, description: req.params.description, songs: req.params.songs}
+
+
+    User.update(req.authenticatedUser._id, {$pull : { 'playlists': playlist }}, { safe: true }, function(err, user) {
+      if (err) return console.log(err)
+
+      res.json(req.authenticatedUser.playlists)
+    })
+
 }
